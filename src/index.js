a55e5619fd5aae6a69825a378992d55f490954ea
danieldev23 <huydevc23@gmail.com>
2023-09-24 14:54:16 +0700

    Simple Nodejs

--- src/index.js
+++ src/index.js
@@ -3,30 +3,31 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = 3000;
+const route = require('./routes');
 
 const exphbs  = require('express-handlebars');
 const hbs = exphbs.create({
     extname: '.hbs'
 }); 
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
 // app.use(morgan('combined'));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // Templates engine
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
+ 
+// Action ---> Dispatcher ---> Function Handler 
 
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/search', (req, res) => {
-    console.log(req.query.q)
-    res.render('search');
-});
-app.get('/news', (req, res) => {
-    res.render('news');
-});
+route(app);
+
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
